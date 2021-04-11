416328aa43d7e219a2ff47dd5d6f70d531a52595
Maniteja Krishnam <mnkr@senncom.com>
2021-04-11 20:16:32 +0530

    python script to fetch meta data based on the key user provided

---
+++ script.py
@@ -0,0 +1,9 @@
+from ec2_metadata import ec2_metadata
+  
+def retrieve_metadata(data_key):
+    print("Meta data for the key provided is")
+    print(getattr(ec2_metadata, data_key))
+
+data_key = raw_input("Provide the key to be fetched from instance metadata: ")
+print("The key provided by the user is ", data_key)
+retrieve_metadata(data_key)
